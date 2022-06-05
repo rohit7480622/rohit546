ea3323c4f463826244dec5d761e642882b84d35c
rohit7480622 <106919840+rohit7480622@users.noreply.github.com>
2022-06-05 22:11:02 +0500

    Add files via upload

---
+++ fsf.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+
+
+
+void *ptr(char *string){
+	
+	void*pt=new char [10];
+	
+	for(int i=0; i<10; i++)
+
+	
+	return pt;
+}
+
+int main(){
+	
+	
+void*P=ptr();
+
+	
+
+	
+	return 0;
+}
